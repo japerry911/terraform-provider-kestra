0ad8e5bff754d6e31bb80205e04872bb8c0393f7
Jack P <jack.e.perry@outlook.com>
2024-06-24 23:04:55 -0600

    fix

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/kestra-io/terraform-provider-kestra/internal/provider"
+	"github.com/japerry911/terraform-provider-kestra/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
